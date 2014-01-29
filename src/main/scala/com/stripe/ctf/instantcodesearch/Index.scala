a705220db4adf4463cdcd8876e6432fab0259d7a
Brett Wilkins <brett@brett.geek.nz>
2014-01-29 21:36:31 +1100

    Removing tris etc *le tear*

--- src/main/scala/com/stripe/ctf/instantcodesearch/Index.scala
+++ src/main/scala/com/stripe/ctf/instantcodesearch/Index.scala
@@ -8,22 +8,12 @@ import scala.collection.mutable.MutableList._
 
 class Index(repoPath: String) {
   val lines = HashMap[(String, Int), String]()
-  val words = HashMap[String, MutableList[(String, Int)]]()
 
   def path() = repoPath
 
   def addFile(file: String, text: String) = {
     text.lines.zipWithIndex.foreach {
-      case (line, line_number) =>
-        lines( (file, line_number+1) ) = line
-        line.split(" ").foreach {
-        word => word.sliding(3).foreach {
-          tri => if (!words.contains(tri)) {
-            words(tri) = new MutableList()
-          }
-          words(tri) += ((file, line_number+1))
-        }
-      }
+      case (line, line_number) => lines( (file, line_number+1) ) = line
     }
   }
 }
--- src/main/scala/com/stripe/ctf/instantcodesearch/Searcher.scala
+++ src/main/scala/com/stripe/ctf/instantcodesearch/Searcher.scala
@@ -18,48 +18,13 @@ class Searcher(index : Index)  {
   val root = FileSystems.getDefault().getPath(index.path)
 
   def search(needle : String, b : Broker[SearchResult]) = {
-    val matches = HashMap[(String, Int), MutableList[String]]()
-    val tris = needle.sliding(3).toList
-    tris.foreach {
-      tri => getMatches(tri).foreach {
-        m => if (matches.contains(m)) {
-            if (!matches(m).contains(tri)) {
-              matches(m) += tri
-            }
-          } else {
-            matches(m) = MutableList(tri)
-          }
+    index.lines.foreach {
+      case (line_key, line) =>
+      if (line.contains(needle)) {
+        b !! new Match(line_key._1, line_key._2);
       }
-    }
-
-    matches.foreach {
-      case (key, tris_found) => // if (tris.forall(tris_found.contains)) {
-        if (checkMatch(needle, key)) {
-          b !! new Match(key._1, key._2);
-        }
-      // }
     }
 
     b !! new Done()
-  }
-
-  def getMatches(needle: String) : Iterable[(String, Int)] = {
-    try {
-        if (index.words.contains(needle)) {
-          return index.words(needle)
-        }
-      } catch {
-        case e: IOException => {
-          return Nil
-        }
-      }
-    return Nil
-  }
-
-  def checkMatch(needle: String, at: (String, Int)) : Boolean = {
-    if (index.lines.contains(at)) {
-      return index.lines(at).contains(needle)
-    }
-    return false
   }
 }
